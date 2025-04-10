95357ddceef4d661819f9a016ce43f77e0049744
posedonYT <vvdard@gmail.com>
2025-04-10 21:27:45 +0700

    ngrok update

--- src/data/backend.js
+++ src/data/backend.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL = 'https://9df0-31-192-239-18.ngrok-free.app'; // Замените на ваш URL бэкенда
+const API_BASE_URL = 'https://6cfc-31-192-239-18.ngrok-free.app'; // Замените на ваш URL бэкенда
 const TG = window.Telegram.WebApp
 
 
